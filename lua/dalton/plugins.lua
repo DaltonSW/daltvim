52459709250c51000c3a97368bfd7660267824da
Dalton Williams <daltonstwilliams@gmail.com>
2024-06-18 17:22:07 -0500

    Remove kickstart remnants, move some kickstart plugins to my folder, set up more config

--- lua/dalton/plugins.lua
+++ lua/dalton/plugins.lua
@@ -21,8 +21,11 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'numToStr/Comment.nvim', -- "gc" to comment visual regions/lines
+  'HiPhish/rainbow-delimiters.nvim',
 
-  -- See `:help gitsigns` to understand what the configuration keys do
+  -- Highlight todo, notes, etc in comments
+  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
+
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
@@ -36,32 +39,7 @@ require('lazy').setup({
     },
   },
 
-  { -- Useful plugin to show you pending keybinds.
-    'folke/which-key.nvim',
-    event = 'VimEnter', -- Sets the loading event to 'VimEnter'
-    config = function() -- This is the function that runs, AFTER loading
-      require('which-key').setup()
-
-      -- Document existing key chains
-      require('which-key').register {
-        ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
-        ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-        ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
-        ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
-      }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
-    end,
-  },
-
-  -- Highlight todo, notes, etc in comments
-  { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
-
+  require 'dalton.plugins.whichkey',
   require 'dalton.plugins.theme',
   require 'dalton.plugins.telescope',
   require 'dalton.plugins.autocomp',
@@ -70,6 +48,9 @@ require('lazy').setup({
   require 'dalton.plugins.neotree',
   require 'dalton.plugins.treesitter',
   require 'dalton.plugins.lsp',
+  require 'dalton.plugins.autopairs',
+  require 'dalton.plugins.debugging',
+  require 'dalton.plugins.indentline',
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
