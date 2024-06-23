b637f2f39efabe8d0f1169f9f293355a783f219c
Dalton Williams <daltonstwilliams@gmail.com>
2024-06-23 18:25:59 -0500

    Updated alpha and whichkey keys a bit

--- lua/dalton/plugins/alpha.lua
+++ lua/dalton/plugins/alpha.lua
@@ -17,9 +17,11 @@ return {
       [[⠀⠀⢸⣿⣿⠀⠀⠀⠀⠀⠀⠀⠀⠀⠀⣿⣿⡇⠀⠀⠀⠀⠀⠀]],
     }
     dashboard.section.buttons.val = {
-      dashboard.button('c', ' .config', ':Neotree ~/.config<CR>'),
+      dashboard.button('c', ' .config', ':Neotree ~/.config<CR>'),
+      dashboard.button('l', ' Lazy', ':Lazy<CR>'),
+      dashboard.button('n', ' neovim', ':Neotree ~/.config/nvim<CR>'),
       dashboard.button('p', ' Projects', ':Neotree ~/Projects<CR>'),
-      dashboard.button('q', '󰅘  Quit NVIM', ':qa<CR>'),
+      dashboard.button('q', '󰅗 Quit NVIM', ':qa<CR>'),
     }
     dashboard.section.footer.val = 'Make Cool Shit and Have Fun'
 
--- lua/dalton/plugins/whichkey.lua
+++ lua/dalton/plugins/whichkey.lua
@@ -6,17 +6,35 @@ return { -- Useful plugin to show you pending keybinds.
     wk.setup()
 
     wk.register {
-      ['leader'] = {
+      ['<leader>'] = {
         a = { '<cmd>Alpha<cr>', '[A]lpha' },
         m = { '<cmd>Mason<cr>', '[M]ason' },
         p = { '<cmd>Lazy<cr>', '[P]lugins' },
-        u = { '<cmd>Undotree<cr>', '[U]ndo Tree' },
+        u = { "<cmd>lua require('undotree').toggle()<cr>'", '[U]ndo Tree' },
+      },
+    }
+
+    wk.register {
+      ['<leader>'] = {
+        d = {
+          name = '[D]iagnostics',
+          -- e = { vim.diagnostic.open_float, "Show [E]rrors"},
+          -- n = { vim.diagnostic.get_next, "Show [E]rrors"},
+
+          -- Diagnostic keymaps
+          -- vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous [D]iagnostic message' })
+          -- vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next [D]iagnostic message' })
+          -- vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Show diagnostic [E]rror messages' })
+          -- vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+        },
       },
     }
     -- Document existing key chains
     wk.register {
       ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
-      ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+      ['<leader>d'] = { name = '[D]iagnostics', _ = 'which_key_ignore' },
+      ['<leader>l'] = { name = '[L]SP Actions', _ = 'which_key_ignore' },
       ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
       ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
       ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
