0e01fd3060be2ee5b3282b02c05457a12515098d
Dalton Williams <daltonstwilliams@gmail.com>
2024-06-23 18:28:40 -0500

    Configured neotree, commented out unused themes

--- lua/dalton/plugins/neotree.lua
+++ lua/dalton/plugins/neotree.lua
@@ -1,10 +1,28 @@
 return {
-    "nvim-neo-tree/neo-tree.nvim",
-    branch = "v3.x",
-    dependencies = {
-      "nvim-lua/plenary.nvim",
-      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-      "MunifTanjim/nui.nvim",
-      "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+  'nvim-neo-tree/neo-tree.nvim',
+  branch = 'v3.x',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+    '3rd/image.nvim', -- Optional image support in preview window: See `# Preview Mode` for more information
+  },
+  config = function()
+    require('neo-tree').setup {
+      filesystem = {
+        filtered_items = {
+          visible = true,
+          show_hidden_count = true,
+          hide_gitignored = false,
+          hide_dotfiles = false,
+          hide_by_name = {
+            -- ???
+          },
+        },
+      },
     }
+    vim.keymap.set('n', '<leader>nn', '<cmd>Neotree focus<cr>', { desc = 'Focus [N]eotree [N]ow' })
+    vim.keymap.set('n', '<leader>nc', '<cmd>Neotree close<cr>', { desc = '[N]eotree [C]lose' })
+    vim.keymap.set('n', '<leader>nf', '<cmd>Neotree reveal<cr>', { desc = '[N]eotree Reveal [F]ile' })
+  end,
 }
--- lua/dalton/plugins/theme.lua
+++ lua/dalton/plugins/theme.lua
@@ -1,6 +1,6 @@
 return {
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
-  { 'Everblush/nvim', name = 'everblush', priority = 999 },
-  { 'polirritmico/monokai-nightasty.nvim', name = 'monokai-nightasty', priority = 999 },
-  { 'folke/tokyonight.nvim', priority = 999 }
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, opts = { transparent_background = true } },
+  -- { 'Everblush/nvim', name = 'everblush', priority = 999 },
+  -- { 'polirritmico/monokai-nightasty.nvim', name = 'monokai-nightasty', priority = 999 },
+  -- { 'folke/tokyonight.nvim', priority = 999 }
 }
