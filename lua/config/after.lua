44c060d36ddfd0a796a95cec7a8b2d9af24e00b8
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-06 10:48:37 -0500

    Moved config loading stuff to config.after

---
+++ lua/config/after.lua
@@ -0,0 +1,7 @@
+require('lsp-zero').on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup()
