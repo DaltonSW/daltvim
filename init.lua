b11fba36c3b36aec4c4a85bf325b3cb8ed5146ca
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-05 20:56:15 -0500

    Catppuccin theme

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ require 'dalton.keymaps'
 
 require 'config.lazy' -- Init Lazy.nvim and load plugins. Should be done after setting leader
 
--- vim.cmd.colorscheme 'catppuccin'
+vim.cmd.colorscheme 'catppuccin'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
