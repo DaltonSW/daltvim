da8e5568a429d2904b0e5f803310192eccbc6a4f
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-05 21:58:59 -0500

    LSP Zero + Mason

--- init.lua
+++ init.lua
@@ -8,6 +8,12 @@ require 'dalton.keymaps'
 
 require 'config.lazy' -- Init Lazy.nvim and load plugins. Should be done after setting leader
 
+require('lsp-zero').on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp_zero.default_keymaps({buffer = bufnr})
+end)
+
 vim.cmd.colorscheme 'catppuccin'
 
 -- The line beneath this is called `modeline`. See `:help modeline`
