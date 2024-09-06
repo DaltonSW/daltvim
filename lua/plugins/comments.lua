869327d1968fb3f6f622a0976dfdd64d90f82947
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-06 11:10:57 -0500

    Added TODO comments

---
+++ lua/plugins/comments.lua
@@ -0,0 +1,31 @@
+return {
+  -- TODO Comments: Colorful keyword comment highlighting
+  {
+    -- Examples:
+    -- TODO:
+    -- NOTE:
+    -- FIX:
+    -- WARN:
+    -- TEST:
+    -- DOC:
+    -- IDEA:
+
+    -- TODO: Different colors? 
+
+    'folke/todo-comments.nvim',
+    event = 'VimEnter',
+    dependencies = { 'nvim-lua/plenary.nvim' },
+    opts = {
+      signs = true,
+      keywords = {
+        TODO = { icon = '󰄲 ', color = '#A3FFE0' },
+        NOTE = { icon = ' ', color = '#D9B3FF', alt = { 'INFO' } },
+        BUG = { icon = '󰃤 ', color = '#FF7878', alt = { 'FIX', 'FIXME', 'ISSUE', 'SLOW' } },
+        WARN = { icon = ' ', color = '#FFB86C', alt = { 'HEY', 'IDEA', 'PERF' } },
+        TEST = { icon = '󰙨 ', color = '#FFA1DC', alt = { 'SETUP', 'TEARDOWN' } },
+        DOC = { icon = '󰈙 ', color = '#D1D1D1', alt = { 'DESC', 'PARAM', 'RETURN' } },
+        IDEA = { icon = ' ', color = '#F1FA8C', alt = { 'TRY', 'PSEUDO' } },
+      },
+    },
+}
+}
