3d78d234eebe83b31514dc663ae58ad2938b516a
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-16 17:49:38 -0500

    Installed octo

--- lua/config/whichkey-groups.lua
+++ lua/config/whichkey-groups.lua
@@ -4,5 +4,8 @@ wk.add {
   { '<leader>f', group = 'Find' },
   { '<leader>s', group = 'Search' },
   { '<leader>n', group = 'Neotree' },
+  { '<leader>o', group = 'Octo' },
+  { '<leader>ol', group = 'Octo List...' },
+  { '<leader>oy', group = 'Octo Yank...' },
   { '<leader>x', group = 'Trouble' },
 }
--- lua/plugins/colors.lua
+++ lua/plugins/colors.lua
@@ -1,7 +1,23 @@
 return {
   -- Catppuccin: Pastel theme
   -- Repo: https://github.com/catppuccin/nvim
-  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000, opts = { transparent_background = true } },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    opts = {
+      transparent_background = true,
+      integrations = {
+        neotree = true,
+        noice = true,
+        neotest = true,
+        mason = true,
+        octo = true,
+        lsp_trouble = true,
+        which_key = true,
+      },
+    },
+  },
 
   -- Highlight Colors: Adds colors to hex codes, HSL codes, CSS variables, etc
   -- Repo: https://github.com/brenoprata10/nvim-highlight-colors
