e34d1f27d5756c777c4a0ab209eba6ca2f20a39d
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-16 11:15:51 -0500

    Warning cleanup. Added border to Lazy

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,11 +2,12 @@ return {
   -- Noice: Replaces messages, cmdline, and popup menu
   -- Repo: https://github.com/folke/noice.nvim
   'folke/noice.nvim',
+  version = '4.4.7', -- HACK: Needing to force this version to fix the cursor blinking issue
   event = 'VeryLazy',
   opts = {},
   dependencies = {
     'MunifTanjim/nui.nvim',
-    'rcarriga/nvim-notify',
+    { 'rcarriga/nvim-notify', opts = { background_colour = '#000000' } },
     'hrsh7th/nvim-cmp',
   },
 }
