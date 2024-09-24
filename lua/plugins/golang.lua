e93317205aea47ed40835a448bd51289d0281a7a
Dalton Williams <daltonstwilliams@gmail.com>
2024-09-24 16:35:30 -0500

    Added gopher.nvim for better Go integration

---
+++ lua/plugins/golang.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    -- Gopher.nvim: Minimal Go development plugin
+    'olexsmir/gopher.nvim',
+    ft = 'go',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'nvim-treesitter/nvim-treesitter',
+    },
+  },
+}
