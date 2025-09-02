f15f93f2eaa8ebc01b6de5b05acc64ab6c10732a
Dalton Williams <email@dalton.dog>
2025-09-02 17:33:36 -0500

    Removed deprecated setting

--- init.lua
+++ init.lua
@@ -14,6 +14,5 @@ require 'config.whichkey-groups'
 
 vim.cmd.colorscheme 'catppuccin'
 
-vim.api.nvim_set_option('clipboard', 'unnamedplus')
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
